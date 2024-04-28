767ad668d483ba04edff3e2f6f27218ecdff4a3d
实验室207 <1619649679@qq.com>
2024-04-28 12:52:21 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'github-origin/master'.
    
    Changes to be committed:
    	new file:   css/common.css
    	new file:   css/goods.css
    	modified:   css/index.css
    	modified:   css/normalize.css
    	new file:   html/goods.html
    	modified:   index.html
    	new file:   js/goods.js
    	new file:   js/index.js
    	new file:   less/common.less
    	new file:   less/goods.less
    	modified:   less/index.less

---
+++ js/goods.js
@@ -0,0 +1,18 @@
+const goods_container = document.querySelector('.goods-container')
+
+for (let i = 0; i < 20; i++) {
+	const s = `<div class="col">
+	<div class="card">
+	  <img
+		src="https://assets-global.website-files.com/5f0e5890acb33b3dec2342e8/626604b4b7815e82d92e12cd_Yellow-Clay-Isometric-Apple-Devices%20ts.jpg"
+		class="card-img-top" alt="...">
+	  <div class="card-body">
+		<h5 class="card-title name text-center">小米电视4A 32英寸</h5>
+		<p class="card-text description text-center">64位四核处理器/1GB+4GB大内存</p>
+		<p class="price text-center">1199元</p>
+	  </div>
+	</div>
+  </div>`
+
+	goods_container.innerHTML += s
+}
