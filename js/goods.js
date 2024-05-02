4a0700a8433f96979f0db7fc92f2a2cfd892890b
实验室207 <1619649679@qq.com>
2024-05-02 21:27:46 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'github-origin/master'.
    
    Changes to be committed:
    	modified:   css/common.css
    	modified:   css/goods.css
    	modified:   js/goods.js
    	modified:   less/common.less
    	modified:   less/goods.less

--- js/goods.js
+++ js/goods.js
@@ -6,11 +6,12 @@ function addEL_addBtn() {
 	const addBtn = document.querySelectorAll('.addBtn')
 
 	addBtn.forEach(btn => btn.addEventListener('click', () => {
-		console.log('click')
 		// e.stopPropagation() // 阻止事件冒泡，防止触发window的scroll事件
 		clickTime++
 		cartCount.innerText = clickTime
 		cartCount.style.display = 'block'
+
+		cart.classList.remove('hide')
 	}))
 }
 
@@ -21,6 +22,8 @@ const modalCloseBtn = document.querySelector('#modal-closeBtn')
 modalCloseBtn.addEventListener('click', () => {
 	clickTime = 0 // 重置点击次数，以关闭购物车弹窗
 	cartCount.style.display = 'none' // 隐藏购物车数量
+
+	cart.classList.add('hide')
 })
 
 
@@ -45,67 +48,282 @@ const sidebar = document.querySelector('.sideBar .list-group')
 
 const herb_info = [
 	[
-		{ name: '茯苓', description: '64位四核处理器', price: '1199元' },
-		{ name: '薏苡仁', description: '64位四核处理器', price: '1199元' },
-		{ name: '丝瓜', description: '64位四核处理器', price: '1199元' },
-		{ name: '冬瓜皮', description: '64位四核处理器', price: '1199元' },
-		{ name: '五指毛桃', description: '64位四核处理器', price: '1199元' },
-		{ name: '陈皮', description: '64位四核处理器', price: '1199元' },
-		{ name: '山药冬瓜汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '赤豆鲤鱼汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '五指毛桃土茯苓煲龙骨', description: '64位四核处理器', price: '1199元' },
-		{ name: '薏米冬瓜骨头汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '茯苓',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '薏苡仁',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://ts1.cn.mm.bing.net/th/id/R-C.838a8b60bd7c9d91182a77a188553a72?rik=d1m8dCrxLnG5LQ&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20190617%2fe57e02640f9a486ea4a904775d5c635c.jpeg&ehk=6xDm95VFF%2bDklNYvy2i%2bZpRdRDcVuVWbANc8TMp1Wxo%3d&risl=&pid=ImgRaw&r=0'
+		},
+		{
+			name: '丝瓜',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '冬瓜皮',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '五指毛桃',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '陈皮',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '山药冬瓜汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '赤豆鲤鱼汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '五指毛桃土茯苓煲龙骨',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '薏米冬瓜骨头汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '黄芪', description: '64位四核处理器', price: '1199元' },
-		{ name: '当归', description: '64位四核处理器', price: '1199元' },
-		{ name: '防风', description: '64位四核处理器', price: '1199元' },
-		{ name: '荆芥', description: '64位四核处理器', price: '1199元' },
-		{ name: '红花', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '黄芪',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '当归',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '防风',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '荆芥',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '红花',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '龙眼肉', description: '64位四核处理器', price: '1199元' },
-		{ name: '阿胶', description: '64位四核处理器', price: '1199元' },
-		{ name: '枸杞', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '龙眼肉',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '阿胶',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '枸杞',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '黄精炖肉', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '黄精炖肉',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '银耳鸡蛋汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '沙参麦冬瘦肉汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '雪羹汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '甲鱼枸杞汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '石斛麦冬汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '银耳鸡蛋汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '沙参麦冬瘦肉汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '雪羹汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '甲鱼枸杞汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '石斛麦冬汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '黄芪炖鸡', description: '64位四核处理器', price: '1199元' },
-		{ name: '参枣汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '参杞羊肉汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '鳝鱼补气汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '西洋参养生汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '黄芪炖鸡',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '参枣汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '参杞羊肉汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '鳝鱼补气汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '西洋参养生汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '赤小豆薏苡仁', description: '64位四核处理器', price: '1199元' },
-		{ name: '车前草赤小豆煲猪小肚', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '赤小豆薏苡仁',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '车前草赤小豆煲猪小肚',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '黑豆红花汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '山楂汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '红花乌鸡汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '黑豆红花汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '山楂汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '红花乌鸡汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '玫瑰花鸡蛋汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '佛手甲鱼汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '菊花鸡肝汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '合欢花猪肝汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '玫瑰解郁汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '玫瑰花鸡蛋汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '佛手甲鱼汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '菊花鸡肝汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '合欢花猪肝汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '玫瑰解郁汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	],
 	[
-		{ name: '当归生姜羊肉汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '核桃人参汤', description: '64位四核处理器', price: '1199元' },
-		{ name: '虫草炖鸡', description: '64位四核处理器', price: '1199元' },
-		{ name: '苁蓉羊肾汤', description: '64位四核处理器', price: '1199元' },
+		{
+			name: '当归生姜羊肉汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '核桃人参汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '虫草炖鸡',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
+		{
+			name: '苁蓉羊肾汤',
+			description: '64位四核处理器',
+			price: '1199元',
+			imgUrl: 'https://img.zcool.cn/community/01f8885c55aa00a801213f26d50e8b.jpg@1280w_1l_2o_100sh.jpg'
+		},
 	]
 ]
 
@@ -118,9 +336,9 @@ function update_herb_info(herb_idx) {
 		const s = `<div class="col">
 			<div class="card">
 			<i class="iconfont icon-add addBtn"></i>
-			<img
-				src="https://assets-global.website-files.com/5f0e5890acb33b3dec2342e8/626604b4b7815e82d92e12cd_Yellow-Clay-Isometric-Apple-Devices%20ts.jpg"
-				class="card-img-top" alt="...">
+			<div class="imgBox">
+				<img src=${herb.imgUrl} class="card-img-top" alt="...">
+			</div>
 			<div class="card-body">
 				<h5 class="card-title name text-center">${herb.name}</h5>
 				<p class="card-text description text-center">${herb.description}</p>
